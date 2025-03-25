b6e7a7c0711efe83b493d7bba87fc672633b1917
Quest_man <mitrofanov551@gmail.com>
2025-03-25 10:58:19 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      'l': {'min': '1600px'},
+
+      'm': {'min': '1024px','max': '1599px'},
+
+      's': {'min': '300px', 'max': '1024px'},
+    
+    extend: {
+      colors:{
+        'grey':'hsl(0, 0%, 20%)'
+      }
+    },
+    }
+  },
+  plugins: [],
+}
+
