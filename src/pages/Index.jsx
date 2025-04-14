d497d2f06688b1f7d3bb3901ab74f4ef7a5ad8e7
Quest_man <mitrofanov551@gmail.com>
2025-04-14 07:25:48 +0300

    Сделал немного фронта

---
+++ src/pages/Index.jsx
@@ -0,0 +1,19 @@
+import Header from "../components/header/Header"
+import FilmCard from "../components/film_cards/FilmCard";
+import Premiere from "../components/premiere/premiere";
+import Features from "../components/Features/Features";
+import Footer from "../components/Footer/Footer";
+import Articles from "../components/articles/Articles";
+function Index() {
+      return (
+        <>
+            <Header/>
+            <Premiere props={{description: "Длинное описание"}}/>
+            <Features/>
+            <Articles/>
+            <Footer/>
+        </>
+      );
+}
+
+export default Index
